1bfe8dd18ead9a97c8de1d6283780cb0acf16f89
Phap <phap.nq214921@sis.hust.edu.vn>
2023-05-23 23:59:01 +0700

    Lab02

---
+++ AimsProject/src/aims/Aims.java
@@ -0,0 +1,18 @@
+package aims;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	Cart anOrder=new Cart();	
+    	DVD dvd1=new DVD("The Lion King","Animation","Roger Aller",87,19.95f);
+    	anOrder.addDVD(dvd1);
+    	DVD dvd2=new DVD("Star Wars","Science Fiction","George Lucas",87,24.95f);
+    	anOrder.addDVD(dvd2);
+    	DVD dvd3=new DVD("Aladin","Animation",18.99f);
+    	anOrder.addDVD(dvd3);
+    	anOrder.remove(dvd1);
+    	System.out.println("Total Cost is:");
+    	System.out.println(anOrder.total_Cost());
+	}
+}
---
+++ Lab02/Cart.java
@@ -0,0 +1,30 @@
+package aims;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED =20;
+	private DVD items_Ordered[]= new DVD[MAX_NUMBERS_ORDERED];
+	int qty_Ordered=0;
+	public void addDVD (DVD disc) {
+		if (qty_Ordered<MAX_NUMBERS_ORDERED) {
+			items_Ordered[qty_Ordered]=disc;
+			qty_Ordered+=1;
+			System.out.println("The disc has been added");
+		}
+		else {System.out.println("The cart is almost full");}
+	}
+	public void remove(DVD disc) {
+        for (int i=0; i< qty_Ordered; i++) {
+            if (items_Ordered[i]== disc) {
+                for (int j= i; j< qty_Ordered- 1; j++) {
+                    items_Ordered[j]= items_Ordered[j+ 1];}
+            }
+        }		
+	}
+	public float total_Cost() {
+		float Cost=0;
+		for (int i=0; i< qty_Ordered; i++) {
+			Cost+= items_Ordered[i].getCost();
+		}
+		return Cost;
+	}
+}
